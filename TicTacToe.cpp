363ea7d65be931b2310c777ab36995ec31b7cb1b
McGeorge24 <steiner.jure@gmail.com>
2024-02-18 20:00:01 +0100

    'AI' now working, beta testing sterted

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -3,7 +3,7 @@
 #include <string.h> //za strcmp()
 //globalne spremenljivke in objecti
 char board[4][4], bot, human;
-int prazne; //blank_spots; globalna
+int prazne; //blank_spots, samo globalna
 struct tPlayer { //za tournament
     int points;
     char name[20];
@@ -11,7 +11,7 @@ struct tPlayer { //za tournament
 struct tCelice {    //za minimax
     int x,y;
 };
-struct tMove {
+struct tMove {      //za minimax
     int x, y, points;
 };
 
@@ -54,7 +54,7 @@ bool validMove (int x, int y) {
 //upošteva velike in male črke
 //če igralec napiše exit vrne false (za konec igre)
 //če je mesto že zasedeno/ni validen ukaz pokliče funkcijo še enkrat
-bool humanMove (char player, int * blank_spots) {
+bool humanMove (char player) {
     char input[6];
     int x,y;
     printf("%c's move:\n", player);
@@ -76,13 +76,13 @@ bool humanMove (char player, int * blank_spots) {
         //če je mesto prosto
         if (validMove(x,y)) {
             board[x][y] = player;
-            *blank_spots--;
+            prazne--;
             return true;
         }
         //če je mesto že zasedeno
         else {
             printf("%s is already occupied...\nChoose another spot:\n", input);
-            return humanMove(player, blank_spots);
+            return humanMove(player);
         }
     }
     //v primeru exit/Exit/esc/ESC
@@ -92,7 +92,7 @@ bool humanMove (char player, int * blank_spots) {
     //vse ostalo
     else {
         printf("Command '%s' does not exist...\nEnter new comand here:\n", input);
-        return humanMove(player, blank_spots);
+        return humanMove(player);
     }
 }
 
@@ -149,76 +149,97 @@ int evaluate (char * state, int depth) {
 void najdiProste(tCelice * proste_celice, char state[4][4]) {
     int y, x, i=0;
     for (y=0;y<3;y++) {
-        for (x=0; x<10; x++) {
+        for (x=0; x<3; x++) {
             if (state[y][x] == ' '){
+                (proste_celice+i)->x = x;
+                (proste_celice+i)->y = y;
                 i++;
-                proste_celice[i].x = x;
-                proste_celice[i].y = y;
             }
         }
     }
 }
 
-tMove minimax(char state[4][4], int depth, char current_player, char other_player) {
+tMove minimax(char state[4][4], int depth, char * current_player, char * other_player) {
     bool game_over;
     tMove best, current;
-    //za primerjavo
-    if (bot == current_player) {
-        best.points = -10000;
+
+    if (bot == *current_player) {    //za kasnejšo primerjavo
+        best.points = -1000000;
         best.x=-1; best.y=-1;
     } else {
-        best.points = 10000;
+        best.points = 1000000;
         best.x=-1; best.y=-1;
     }
-
-    checkStatus(&game_over, &state[0][0], depth);
-    if (game_over) {    //velja tudi če je depth 0, vrne vrednost, glede na to kdo zmaga, glej evaluate()
-        best.points = evaluate(&state[0][0], depth);
+    
+    checkStatus(&game_over, state[0], depth);
+    if (game_over) {                //velja tudi če je depth 0, vrne vrednost, glede na to kdo zmaga, glej evaluate()
+        best.points = evaluate(state[0], depth);
         return best;
     }
 
     tCelice proste_celice[depth];
     najdiProste(proste_celice, state);
+
     for (int i=0; i<depth; i++) {
-        state[proste_celice[i].y][proste_celice[i].x] = current_player;
+        state[proste_celice[i].y][proste_celice[i].x] = *current_player;
         current = minimax(state, depth-1, other_player, current_player); // ju zamenjam zaradi algoritma
         state[proste_celice[i].y][proste_celice[i].x] = ' ';
+
         current.x = proste_celice[i].x;
         current.y = proste_celice[i].y;
-        if (current_player == bot) {
-            if (current.points>best.points) best = current;
+
+        if (*current_player == bot) {
+            if (current.points > best.points) best = current;
         }
-        if (current_player == human) {
-            if (current.points<best.points) best = current;
+        else {
+            if (current.points < best.points) best = current;
         }
     }
     return best;
 }
 
-/*void singleplayer () {
-}*/
+void singleplayer () {
+    bool game_over = false;
+    tMove computer;
+    getchar();
+    printf("Choose X or O (now working X only):\t");
+    scanf("%c", &human);
+    if (human == 'O') bot = 'X';
+    else bot = 'O';
+    prazne = 9;
+    clearBoard(board[0]);
+    printBoard(board[0]);
+    while (!game_over) {
+        if (!humanMove(human)) break;
+        computer = minimax(board, prazne, &bot, &human);
+        prazne--;
+        printf("%d %d\n", computer.y, computer.x);
+        board[computer.y][computer.x] = bot;
+        printBoard(board[0]);
+    }
+}
 
 void duel () {
     char input[6], zmagovalec;
-    //izhod - izhod iz zanke iger, konec_igre - izhod iz ene igre
-    bool running = true, konec_igre, izhod = false;
+    //izhod - izhod iz zanke iger, game_over - izhod iz ene igre
+    bool running = true, game_over, izhod = false;
     //zanka za več iger
     while (running) {
-        konec_igre = false;
+        game_over = false;
         clearBoard(&board[0][0]);
         printBoard(&board[0][0]);
         prazne = 9;
         //zanka za eno igro
-        while (!konec_igre) {
-            izhod = konec_igre = !humanMove('X', &prazne); //zapiše na ploščo in pove ali je player napisal exit...
-            if (konec_igre) break;
+        while (!game_over) {
+            izhod = game_over = !humanMove('X'); //zapiše na ploščo in pove ali je player napisal exit...
+            if (game_over) break;
             printBoard(&board[0][0]);
-            zmagovalec = checkStatus(&konec_igre, &board[0][0], prazne); //pove kdo zmaga in shrani ali je igre konec v spremenljivko...
+            zmagovalec = checkStatus(&game_over, &board[0][0], prazne); //pove kdo zmaga in shrani ali je igre konec v spremenljivko...
             printf("-%c\n", zmagovalec);
-            if (konec_igre) break;
+            if (game_over) break;
 
-            izhod = konec_igre = !humanMove('O', &prazne);
-            zmagovalec = checkStatus(&konec_igre, &board[0][0], prazne);
+            izhod = game_over = !humanMove('O');
+            zmagovalec = checkStatus(&game_over, &board[0][0], prazne);
             printBoard(&board[0][0]);
             printf("-%c\n", zmagovalec);
         }
@@ -272,10 +293,10 @@ int tournament () {
 
 int main() {
     int game_mode;
-    printf("Game mode (enter 1-3):\n1.\tSingleplayer (comming soon)\n2.\tDuel\n3.\tTournament (comming soon)\n");
+    printf("Game mode (enter 1-3):\n1.\tSingleplayer (BETA)\n2.\tDuel\n3.\tTournament (comming soon)\n");
     scanf("%d", &game_mode);
     printf("-----------------------\n   Type exit to exit\n-----------------------\n");
-    //if (game_mode == 1) singleplayer();
+    if (game_mode == 1) singleplayer();
     if (game_mode == 2) duel();
     if (game_mode == 3) tournament();
     printf ("Thanks for playing!");
